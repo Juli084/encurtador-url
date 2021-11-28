d53a94f8260cdb926de739c43781c597d9e8bc6a
Juli084 <juliorodrigues84@live.com>
2021-11-28 12:56:34 -0300

    repositorio inicial encurtador url

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+    mongodb+srv://juli084:<password>@url.m0ohe.mongodb.net/myFirstDatabase?retryWrites=true&w=majority,
+}
